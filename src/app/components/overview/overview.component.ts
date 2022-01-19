e3c0f2dc9a37a643b7daa73f643f60b1bb4d7e6e
DanGreat <dbassey360@gmail.com>
2022-01-19 14:57:20 +0100

    Added paging to table

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RequestService } from 'src/app/services/request.service';
-import * as Highcharts from 'highcharts';
+
 
 @Component({
   selector: 'app-overview',
@@ -9,8 +9,7 @@ import * as Highcharts from 'highcharts';
 })
 export class OverviewComponent implements OnInit {
 
-  highcharts = Highcharts;
-  chartOptions = {};
+ 
 
   searchTerm = '';
   page = 1;
@@ -31,37 +30,28 @@ export class OverviewComponent implements OnInit {
       this.allTodos = this.todos;
     });
 
-    this.chartOptions = {
-      title: {
-        text: ""
-      },
-      xAxis: {
-        title: {
-          text: 'Month'
-        },
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-      },
-      yAxis: {
-        title: {
-          text: "Volume"
-        }
-      },
-      series: [{
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 24.4, 19.3, 16.0, 18.4, 17.9],
-        type: ''
-      }]
-    }
   }
 
 
   search() {
-    console.log('All Todos: ', this.allTodos);
-    
+
     if(this.searchTerm) {
       const value = this.searchTerm;
       this.todos = this.allTodos.filter((val: any) => val['title'].toLowerCase().includes(value));
       this.collectionSize = this.todos.length;
+    }
+    
+  }
+
+  filterCompleted(ev: any) {
+    const value = ev.target.value
+
+    if(value === 'true') {
+      this.todos = this.allTodos.filter((val: any) => val['completed'] === true );
+    } else if(value === 'false') {
+      this.todos = this.allTodos.filter((val: any) => val['completed'] === false );
+    } else {
+      this.todos = this.allTodos;
     }
     
   }
--- src/app/shared/chart/chart.component.ts
+++ src/app/shared/chart/chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartDataset, ChartOptions } from 'chart.js';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-chart',
@@ -8,34 +8,71 @@ import { ChartDataset, ChartOptions } from 'chart.js';
 })
 export class ChartComponent implements OnInit {
 
-  legend = false;
-  delayed: any;
-
-  public lineChartData: ChartDataset[] = [
-    { 
-      data: [65, 59, 80, 81, 56, 55, 40],
-      label: 'Series A',
-      backgroundColor: [
-        'rgba(54, 162, 235, 0.2)',
-      ],
-      borderColor: [
-        'rgba(54, 162, 235, 0.2)',
-      ],
-      borderWidth: 0
-   },
-  ];
-  public lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions  = {
-    responsive: true,
-    layout: {
-      padding: 20
-    }
-  };
+  highcharts = Highcharts;
+  chartOptions = {};
 
   public lineChartLegend = false;
   public lineChartPlugins = [];
 
-  constructor() { }
+  constructor() { 
+    
+    this.chartOptions = {
+      chart: {
+        type: 'areaspline'
+      },
+      title: {
+        text: ""
+      },
+      xAxis: {
+        title: {
+          text: 'Month'
+        },
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+      },
+      yAxis: {
+        title: {
+          text: "Volume"
+        }
+      },
+      plotOptions: {
+        areaspline: {
+            fillOpacity: 0.5
+        }
+      },
+      series: [{
+        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 24.4, 19.3, 16.0, 18.4, 17.9],
+        type: ''
+      }],
+      responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            // Make the labels less space demanding on mobile
+            // chartOptions: {
+            //     xAxis: {
+            //         labels: {
+            //             formatter: function () {
+            //                 return this.value.charAt(0);
+            //             }
+            //         }
+            //     },
+            //     yAxis: {
+            //         labels: {
+            //             align: 'left',
+            //             x: 0,
+            //             y: -2
+            //         },
+            //         title: {
+            //             text: ''
+            //         }
+            //     }
+            // }
+        }]
+      }
+    }
+  }
 
   ngOnInit(): void {
   }
